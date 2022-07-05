6f480a1096f7805202771946db8ba3d0d0b4f6e1
Wellynton Delfino <wnodelfino@gmail.com>
2022-07-05 08:59:49 -0300

    subindo alterações

---
+++ src/main/java/br/com/springboot/models/User.java
@@ -0,0 +1,79 @@
+package br.com.springboot.models;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+  private Long id;
+  private String name;
+  private String username;
+  @JsonIgnore
+  private String password;
+  private Date created_at;
+  private Date updated_at;
+  private Date deleted_at;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Date getCreated_at() {
+    return created_at;
+  }
+
+  public void setCreated_at(Date created_at) {
+    this.created_at = created_at;
+  }
+
+  public Date getUpdated_at() {
+    return updated_at;
+  }
+
+  public void setUpdated_at(Date updated_at) {
+    this.updated_at = updated_at;
+  }
+
+  public Date getDeleted_at() {
+    return deleted_at;
+  }
+
+  public void setDeleted_at(Date deleted_at) {
+    this.deleted_at = deleted_at;
+  }
+
+}
